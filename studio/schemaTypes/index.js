1d47830813bdf3f75a869aeb97f84e19fa684864
Akbarali98 <115330712+Akbarali98@users.noreply.github.com>
2026-04-08 07:58:14 -0400

    Update studio/schemaTypes/index.js

--- studio/schemaTypes/index.js
+++ studio/schemaTypes/index.js
@@ -2,5 +2,6 @@ import project from './project'
 import siteSettings from './siteSettings'
 import homePage from './homePage'
 import privacyPolicy from './privacyPolicy'
+import blogPost from './blogPost'
 
-export const schemaTypes = [siteSettings, homePage, project, privacyPolicy]
+export const schemaTypes = [siteSettings, homePage, project, blogPost, privacyPolicy]
